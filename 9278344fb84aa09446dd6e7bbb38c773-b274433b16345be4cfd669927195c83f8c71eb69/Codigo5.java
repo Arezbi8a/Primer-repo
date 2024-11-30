0d561e6f78b784ca11f59735cb96bbf201c1aba8
Arezbi8a <arezbi8@ciencias.unam.mx>
2024-11-29 19:27:52 -0600

    codigo de otros 5

--- 9278344fb84aa09446dd6e7bbb38c773-b274433b16345be4cfd669927195c83f8c71eb69/Codigo5.java
+++ 9278344fb84aa09446dd6e7bbb38c773-b274433b16345be4cfd669927195c83f8c71eb69/Codigo5.java
@@ -1,21 +1,23 @@
 public class Codigo5 {
 
-	    Scanner s = new Scanner();
-	    System.out.print("Introduzca un número: ');
+	public static void main(String[] arg) {   //Agregamos el static
+
+	    Scanner s = new Scanner(System.in);//Agregamos System.in
+	    System.out.print("Introduzca un número: "); //Faltan comillas
 	    String ni = s.nextLine();
-	    int c = ni;
+	    int c = Integer.parseInt(ni); //Debemos cambiar "ni" con parseInt para que c sea un int
 	    
 	    int afo = 0;
 	    int noAfo = 0;
 	    
-	    while (ni > 0) {
-		  int digito = (int)(ni % 10);
+	    while (c > 0) { //Cambiar a c todo lo que este con ni
+		  int digito = (int)(c % 10);
 	      if ((digito == 3) || (digito == 7) || (digito == 8) || (digito == 9)) {
 			afo++;
 	      } else {
 			noAfo++;
-		  
-		  ni /= 10;
+		  } //Necesitamos esta llave para que no sea un loop infinito
+		  c /= 10;
 	    }
 
 	    if (afo > noAfo) {
